ae4dae35be83b06de77e74423dc65394f29693cc
Pierre Obregon <pierreobregon@prima.com.pe>
2024-12-18 10:13:24 -0500

    Primer commit

---
+++ src/main/java/com/poc_kuntur17_oci/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.poc_kuntur17_oci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
